04646fce5541b2fd30ef9b04453a5f95e14f6df9
Kai / Kai Akahane <k.akahane.24.7.1548@4nonome.com>
2024-08-31 22:56:14 +0900

    制御構文の解答

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,28 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+a = 61
+if a <= 1:
+    print(False)
+else:
+    for i in range(2, int(a**0.5)+1):
+        if a % i == 0:
+            print(False)
+            break
+    else:
+        print(True)
+
+b = 10
+if b <= 1:
+    print(False)
+else:
+    for i in range(2, int(b**0.5)+1):
+        if b % i == 0:
+            print(False)
+            break
+    else:
+        print(True)
+        
+
+
+
