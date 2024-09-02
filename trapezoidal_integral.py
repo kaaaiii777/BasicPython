d792a37abb1855ae014a3b24268db52b049e0564
Kai / Kai Akahane <k.akahane.24.7.1548@4nonome.com>
2024-09-02 09:58:32 +0900

    制御構文の解答

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,18 @@
-from math import sin
+from math import sin,pi
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
+
+a = 0
+b = pi / 2
+N = 100
+h = (b - a) / N
+Sk = 0
+S = 0
+
+for k in range(1,N+1):
+    Sk = 0.5 * h * (sin(a + (k - 1) * h) + sin(a + k * h))
+    S += Sk
+
+print(S)
